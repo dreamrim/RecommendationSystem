59524d82b55fbd5f48448cc56f712da59a317b82
Fry Hsueh <fryhsueh@gmail.com>
2021-08-16 22:08:21 +0800

    replace ugly filters with functional interface and adopt convenient computeIfAbsent()

--- week1/StepOneStarterProgram/FirstRatings.java
+++ week1/StepOneStarterProgram/FirstRatings.java
@@ -3,16 +3,18 @@
  * 在这里给出对类 FirstRatings 的描述。
  * 
  * @作者（你的名字）
- * @版本（一个版本号或者一个日期）
+ * @ v0.1
  */
 import edu.duke.*;
 import java.util.*;
+import java.util.function.Predicate;
+
 import org.apache.commons.csv.*;
 
 public class FirstRatings {
     
     public ArrayList<Movie> loadMovies(String filename){
-        ArrayList<Movie> movies = new ArrayList<Movie>();
+        ArrayList<Movie> movies = new ArrayList<>();
         FileResource fr = new FileResource(filename);
         CSVParser parser = fr.getCSVParser();
         for (CSVRecord record : parser){
@@ -28,60 +30,29 @@ public class FirstRatings {
         }
         return movies;
     }
-    
-    public int num(ArrayList<Movie> movies, Filter f){
+    private interface consumer<M, K, V> {
+        void accpet(M m, K k, V v);
+    }
+
+
+    // get num of movies matching specific condition
+    public <T> int  num(ArrayList<T> movies, Predicate<T> condition) {
         int num = 0;
-        for (Movie movie : movies){
-            if (f.satisfies(movie)){
+        for (T movie : movies) {
+            if (condition.test(movie)) {
                 num++;
             }
         }
         return num;
     }
-    
-    public int num(ArrayList<Rater> movies, rFilter f){
-    int num = 0;
-    for (Rater movie : movies){
-        if (f.satisfies(movie)){
-            num++;
-        }
-    }
-    return num;
-    }
-    
-    
-    public int num(mFilter f){
-        int num = 0;
-        int max = 0;
-        HashMap<String, ArrayList<String>> map = f.getMap();
-        HashMap<Integer, ArrayList<String>> list = new HashMap<Integer,ArrayList<String>>();
-        for (String key : map.keySet()){
-            int size = map.get(key).size();
-            //System.out.println(map.get(key));
-            if (!list.containsKey(size)){
-                ArrayList<String> name = new ArrayList<String>();
-                name.add(key);
-                list.put(size, name);
-            }
-            else{
-                list.get(size).add(key);
-            }
-            if (size > max){
-                max = size;
-                num++;
-            }
-        }
-        System.out.println("max is " + max + " name is "+list.get(max));
-        System.out.println("only one are " + list.get(1).size());
-        return list.get(num).size();
-    }
+
+
     public ArrayList<Rater> loadRaters(String filename){
-        ArrayList<Rater> raters = new ArrayList<Rater>();
+        ArrayList<Rater> raters = new ArrayList<>();
         FileResource fr = new FileResource(filename);
         CSVParser parser = fr.getCSVParser();
-        ArrayList<String> list = new ArrayList<String>();
-        ArrayList<String> movielist = new ArrayList<String>();
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> movielist = new ArrayList<>();
         int index = -1;
         int num = 0;
         for (CSVRecord record : parser){
@@ -109,46 +80,73 @@ public class FirstRatings {
         System.out.println("there are "+num+" movies");
         return raters;
     }
+
+
+    private void getMap(ArrayList<Movie> movies, consumer<HashMap<String, Collection<String>>, String, Movie> consumer) {
+        HashMap<String, Collection<String>> map = new HashMap<>();
+        for (Movie movie : movies){
+            for (String director : movie.getDirector().split(",")){
+               // map.computeIfAbsent(director, k -> new ArrayList<>()).add(movie.getTitle());
+                consumer.accpet(map, director, movie);
+            }
+        }
+        int num = 0;
+        int max = 0;
+        HashMap<Integer, ArrayList<String>> list = new HashMap<>();
+        for (String key : map.keySet()){
+            int size = map.get(key).size();
+            list.computeIfAbsent(size, k -> new ArrayList<>()).add(key);
+            if (size > max){
+                max = size;
+                num++;
+            }
+        }
+        System.out.println("max is " + max + " name is "+list.get(max));
+        System.out.println("only one are " + list.get(1).size());
+    }
     
     public void testLoadMovies(){
         //ArrayList<Movie> movies = loadMovies("data/ratedmovies_short.csv");
         ArrayList<Movie> movies = loadMovies("data/ratedmoviesfull.csv");
         //System.out.println(movies);
-        Filter f = new genreFilter("Comedy");
-        System.out.println(num(movies, f));
-        f = new minuteFilter(150);
-        System.out.println(num(movies, f));
-        mFilter mf = new theMostMovieFilter(movies);
-        System.out.println(num(mf));
-        mf = new theMostDirectorFilter(movies);
-        System.out.println(num(mf));
+        // num of Comedy
+        System.out.println("Comedy: " + num(movies, movie -> movie.getGenres().contains("Comedy")));
+
+        // num of movies greater than 150min in length
+        System.out.println(num(movies, movie -> movie.getMinutes() >= 150));
+
+        //maximum number of movies by any director
+        getMap(movies, (map, director, movie) -> map.computeIfAbsent(movie.getTitle(), x -> new ArrayList<>()).add(director)
+        );
+
+        getMap(movies, (map, director, movie) -> map.computeIfAbsent(director, x -> new ArrayList<>()).add(movie.getTitle())
+        );
     }
     
     public void testLoadRaters(){
         //ArrayList<Rater> raters = loadRaters("data/ratings_short.csv");
         ArrayList<Rater> raters = loadRaters("data/ratings.csv");
-        HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
+        HashMap<Integer, ArrayList<String>> map = new HashMap<>();
         int max = 0;
         for (Rater rater : raters){
             int ratings = rater.numRatings();
             System.out.println(rater.getID() +" "+ ratings +" "+ rater.getItemsRated());
-            if (map.containsKey(ratings)){
-                map.get(ratings).add(rater.getID());
-            }
-            else{
-                ArrayList<String> id = new ArrayList<String>();
-                id.add(rater.getID());
-                map.put(ratings, id);
-            }
+            map.computeIfAbsent(ratings, x -> new ArrayList<>()).add(rater.getID());
             if (ratings > max){
                 max = ratings;
             }
         }
         System.out.println(map.get(max));
         System.out.println("num of raters is " + map.get(max).size() + " ratings is " + max);
-        rFilter f = new raterFilter("193");
-        System.out.println(num(raters, f));
-        f = new movieFilter("1798709");
-        System.out.println(num(raters, f));
+
+        System.out.println(num(raters, x -> x.getID().equals("193")));
+
+        System.out.println(num(raters, x -> x.hasRating("1798709") ));
+    }
+
+    public static void main(String[] args) {
+        FirstRatings fr = new FirstRatings();
+        // fr.testLoadMovies();
+        fr.testLoadRaters();
     }
 }
